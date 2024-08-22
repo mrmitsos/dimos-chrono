cfea2d285a23a53dd7d3cf7da8ebcc265c91762f
mrmitsos <113947771+mrmitsos@users.noreply.github.com>
2024-08-22 04:23:40 +0300

    Add files via upload

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,6 +1,7 @@
 import InitalLoadActiveUsers from "@/components/InitalLoadActiveUsers";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
