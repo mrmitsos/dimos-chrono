f03c25cee70a08b1ab7b5b068a3c07364e090e9b
Miltos <dai17079@uom.edu.gr>
2025-10-23 17:56:53 +0300

    page updated

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,4 +1,4 @@
-import InitalLoadActiveUsers from "@/components/InitalLoadActiveUsers";
+import InitalLoadActiveUsers from "../components/InitalLoadActiveUsers";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
